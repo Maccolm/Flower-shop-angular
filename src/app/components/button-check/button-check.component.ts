b83c64cfe4eb4c94519d9c4a65d1ebc262d07f8c
Vitalii Shapovalov <martyit@ukr.net>
2025-01-15 17:09:16 +0100

    add ButtonCheck component with PrimeNG integration and update styles

---
+++ src/app/components/button-check/button-check.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+  selector: 'app-button-check',
+  standalone: true,
+  imports: [ButtonModule],
+  templateUrl: './button-check.component.html',
+  styleUrl: './button-check.component.scss'
+})
+export class ButtonCheckComponent {
+
+}
