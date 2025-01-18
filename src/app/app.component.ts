cad5e5472501ddffdd4048da781b6bb64ed6738c
Vitalii Shapovalov <martyit@ukr.net>
2025-01-18 10:57:32 +0100

    layout about row in Main component with dynamic content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonCheckComponent } from './components/button-check/button-check.component';
 
 @Component({
     selector: 'app-root',
 	 standalone: true,
-    imports: [RouterOutlet, ButtonCheckComponent],
+    imports: [RouterOutlet],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,18 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent implements OnInit {
-ngOnInit(): void {
-	const aboutObj: object = [
-		{
-			imgPath: '../../public/icons/custom-icon-1.png'
-		}
-	]
-}
+	public aboutObj: any[] = [
+	  {
+		  imgPath: '/img/icons/custom-icon-1.png',
+		  title: 'Leaders Of Tomorrow',
+		  text: 'We inspire the world with beauty and set trends in floristry.'
+	  },
+	  {
+		  imgPath: '/img/icons/custom-icon-2.png',
+		  title: 'Powerful Solutions',
+		  text: 'Every flower is our answer to your dreams and special moments.'
+	  },
+	  {
+		  imgPath: '/img/icons/custom-icon-3.png',
+		  title: 'Ready To Be Impressed',
+		  text: 'Experience the magic crafted by professionals for your most cherished occasions.'
+	  }
+  ]
+	ngOnInit(): void {
+	}
 }
