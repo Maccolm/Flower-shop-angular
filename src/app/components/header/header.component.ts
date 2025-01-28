b04e857b40b527bd84ac7bceb2ed970467b082d8
Vitalii Shapovalov <martyit@ukr.net>
2025-01-28 19:02:35 +0100

    add Header component with menu items and styles; integrate into Main component; adjust padding in Parallax and Main styles for improved layout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MenubarModule, RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-
+	menuItems = [
+		{
+			label: 'HOME',
+			href: '/'
+		},
+		{
+			label: 'PRODUCT LIST',
+			href:'/products',
+		},
+		{
+			label: 'ABOUT US',
+			href:'/about',
+		},
+		{
+			label: 'CONTROL PANEL',
+			items: [
+				{
+					label: 'ADD PRODUCT',
+					href: 'products/product-form'
+				},
+				{
+					label: 'USERS',
+					href: 'users'
+				},
+				{
+					label: 'USERS TYPES',
+					href:'users_types/list'
+				}
+			]
+		},
+	]
 }
